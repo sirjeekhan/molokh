52221088490aeece63c99b805feafc4200aa070a
mohsin <mr.sirjee@gmail.com>
2021-11-10 21:05:14 +0500

    without secret key

--- main.tf
+++ main.tf
@@ -1,9 +1,8 @@
 provider "aws"{
 region = "us-east-1"
-access_key = "AKIAQHIV3PCL6VZD4MU4"
-secret_key = "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"
+access_key = ""
+secret_key = ""
 }
-
 resource "aws_instance" "ForTerraform" {
   ami = "ami-0416f96ae3d1a3f29"
   instance_type = "t2.micro"
